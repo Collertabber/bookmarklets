dfe20b3af9bfe32c2db3a47ad954778fbd6bf65d
Collertabber <84198237+Collertabber@users.noreply.github.com>
2022-10-25 12:15:09 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({});
+const app = express();
+
+app.get('*', (req, res) => {
+
+    console.log('Request', req.method, req.url);
+    proxy.web(req, res, { target: `${req.protocol}://${req.hostname}` });
+});
+
+app.listen(3000, () => console.log('Application started on port 3000'));
